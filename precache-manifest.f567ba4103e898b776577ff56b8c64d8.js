de48255e1dba16528f5ecaace816c4854dba20c2
JimmeyCheung <100178981@qq.com>
2020-08-17 13:55:06 +0800

    deploy

---
+++ precache-manifest.f567ba4103e898b776577ff56b8c64d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0789e8001e0de51548cc056fa224e773",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "73b43ad7988e69048e7e",
+    "url": "/morney-react-website/static/css/2.d684ea13.chunk.css"
+  },
+  {
+    "revision": "96462c8bb0994d4b2aa1",
+    "url": "/morney-react-website/static/css/main.ef6c72ad.chunk.css"
+  },
+  {
+    "revision": "73b43ad7988e69048e7e",
+    "url": "/morney-react-website/static/js/2.0f1255ad.chunk.js"
+  },
+  {
+    "revision": "0b21de18640ccf7c15349b5dd11613b9",
+    "url": "/morney-react-website/static/js/2.0f1255ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96462c8bb0994d4b2aa1",
+    "url": "/morney-react-website/static/js/main.47768eb1.chunk.js"
+  },
+  {
+    "revision": "82160e8b91ac99149233",
+    "url": "/morney-react-website/static/js/runtime-main.d1edae06.js"
+  }
+]);
